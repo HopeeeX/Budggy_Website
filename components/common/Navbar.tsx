98adda31cbd57f5e25c9cc033bb53feccb2e56ff
Jofer Usa <joferusa43@gmail.com>
2024-05-20 21:56:17 +0800

    add the color change in header

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,89 +1,92 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
 import { FaLinkedin, FaXTwitter, FaInstagram } from "react-icons/fa6";
 import DropDown from './buttons/DropDown';
 
 const NavBar = () => {
-    const [scrolling, setScrolling] = useState(false);
+    const [color, setColor] = useState('');
 
     useEffect(() => {
-        console.log("Test")
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
+        const changeColor = () => {
+            if (window.scrollY >= 20) {
+                setColor('bg-white');
+            } else {
+                setColor('');
+            }
+        };
+        window.addEventListener('scroll', changeColor);
+
+        // Cleanup function to remove the event listener
+        return () => {
+            window.removeEventListener('scroll', changeColor);
+        };
     }, []);
 
-    const handleScroll = () => {
-        if (window.scrollY > 20) {
-            setScrolling(true);
-        } else {
-            setScrolling(false);
-        }}
-        
-        return (
-            <nav className={scrolling ? 'z-10 w-full h-[123px] lg:h-[88px] flex flex-col justify-center items-center px-[10px] sm:px-[20px] lg:px-0 fixed bg-white' : 'z-10 w-full h-[123px] lg:h-[88px] flex flex-col justify-center items-center px-[10px] sm:px-[20px] lg:px-0 fixed bg-transparent'}>
-                <div className='w-full flex flex-col justify-center items-center gap-[12px]'>
-                    {/*Multilingual-sm-Screen*/}
-                    <div className='w-full flex flex-col justify-center items-end lg:hidden'>
-                        <div className='flex flex-row gap-2'>
-                            <button className='text-[15px] underline'>English</button>
-                            <button className='text-[15px] underline'>العربية</button>
-                        </div>
+    return (
+        <nav className={`z-10 w-full h-[123px] lg:h-[88px] flex flex-col justify-center items-center px-[10px] sm:px-[20px] lg:px-0 fixed transition-colors duration-500 ease-in-out ${color}`}>
+            <div className='w-full flex flex-col justify-center items-center gap-[12px]'>
+                {/*Multilingual-sm-Screen*/}
+                <div className='w-full flex flex-col justify-center items-end lg:hidden'>
+                    <div className='flex flex-row gap-2'>
+                        <button className='text-[15px] underline'>English</button>
+                        <button className='text-[15px] underline'>العربية</button>
                     </div>
-                    <div className="w-full flex flex-row justify-between lg:justify-center items-center lg:gap-[216px] xl:gap-[598px]">
-                        {/*Logo*/}
-                        <div className="w-[114px] h-[50px] lg:w-[147px] lg:h-[64px] hidden sm:flex relative">
-                            <Image
-                                src={"/assets/images/budggy-primary-logo.png"}
-                                layout="fill"
-                                objectFit='contain'
-                                quality={100}
-                                alt='budggy-logo'
-                            />
-                        </div>
-                        <div className="flex sm:hidden w-[40px] h-[40px] relative">
-                            <Image
-                                src={"/assets/images/logo-mobile.png"}
-                                layout="fill"
-                                objectFit='contain'
-                                quality={100}
-                                alt='budggy-logo'
-                            />
-                        </div>
-                        {/*Details*/}
-                        <div className="flex flex-row gap-[24px]">
-                            {/*Socials*/}
-                            <div className="hidden sm:flex flex-row justify-center items-center gap-[6px]">
-                                <div>
-                                    <h2 className="text-black font-semibold text-[14px]">Follow us on</h2>
-                                </div>
-                                <div className="flex flex-row gap-[16px] justify-center items-center">
-                                    <a href="https://www.linkedin.com/company/budggy/">
-                                        <FaLinkedin size={21} color="#050505" />
-                                    </a>
-                                    <a href="https://twitter.com/BudggyHQ">
-                                        <FaXTwitter size={21} color="#050505" />
-                                    </a>
-                                    <a href="https://www.instagram.com/budggyhq/">
-                                        <FaInstagram size={23} color="#050505" />
-                                    </a>
-                                </div>
-                            </div>
-                            {/*Button*/}
+                </div>
+                <div className="w-full flex flex-row justify-between lg:justify-center items-center lg:gap-[216px] xl:gap-[598px]">
+                    {/*Logo*/}
+                    <div className="w-[114px] h-[50px] lg:w-[147px] lg:h-[64px] hidden sm:flex relative">
+                        <Image
+                            src={"/assets/images/budggy-primary-logo.png"}
+                            layout="fill"
+                            objectFit='contain'
+                            quality={100}
+                            alt='budggy-logo'
+                        />
+                    </div>
+                    <div className="flex sm:hidden w-[40px] h-[40px] relative">
+                        <Image
+                            src={"/assets/images/logo-mobile.png"}
+                            layout="fill"
+                            objectFit='contain'
+                            quality={100}
+                            alt='budggy-logo'
+                        />
+                    </div>
+                    {/*Details*/}
+                    <div className="flex flex-row gap-[24px]">
+                        {/*Socials*/}
+                        <div className="hidden sm:flex flex-row justify-center items-center gap-[6px]">
                             <div>
-                                <button className="bg-[#F79400] w-[142px] h-[51px] rounded-xl text-white text-[16px] font-medium">
-                                    Get in Touch
-                                </button>
+                                <h2 className="text-black font-semibold text-[14px]">Follow us on</h2>
                             </div>
-                            {/*Multilingual-lg-Screen*/}
-                            <div className='hidden lg:flex'>
-                                <DropDown />
+                            <div className="flex flex-row gap-[16px] justify-center items-center">
+                                <a href="https://www.linkedin.com/company/budggy/">
+                                    <FaLinkedin size={21} color="#050505" />
+                                </a>
+                                <a href="https://twitter.com/BudggyHQ">
+                                    <FaXTwitter size={21} color="#050505" />
+                                </a>
+                                <a href="https://www.instagram.com/budggyhq/">
+                                    <FaInstagram size={23} color="#050505" />
+                                </a>
                             </div>
+                        </div>
+                        {/*Button*/}
+                        <div>
+                            <button className="bg-[#F79400] w-[142px] h-[51px] rounded-xl text-white text-[16px] font-medium">
+                                Get in Touch
+                            </button>
+                        </div>
+                        {/*Multilingual-lg-Screen*/}
+                        <div className='hidden lg:flex'>
+                            <DropDown />
                         </div>
                     </div>
                 </div>
-            </nav>
-        )
-    }
+            </div>
+        </nav>
+    );
+};
 
-    export default NavBar
+export default NavBar;
