798ab74267a8839b74e3946bbe50de325768cdda
Christine Hope <hope.x@budggy.com>
2024-05-22 19:24:38 +0800

    Add logo size animation in navbar and change the button size in sm-screen

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -7,20 +7,26 @@ import Link from 'next/link';
 
 const NavBar = () => {
     const [color, setColor] = useState('');
+    const [hideMultilingual, setHideMultilingual] = useState(false);
+    const [scrolling, setScrolling] = useState(false);
 
     useEffect(() => {
-        const changeColor = () => {
+        const handleScroll = () => {
             if (window.scrollY >= 20) {
-                setColor('bg-white');
+                setColor('bg-white bg-opacity-90');
+                setHideMultilingual(true);
+                setScrolling(true);
             } else {
                 setColor('');
+                setHideMultilingual(false);
+                setScrolling(false);
             }
         };
-        window.addEventListener('scroll', changeColor);
+        window.addEventListener('scroll', handleScroll);
 
         // Cleanup function to remove the event listener
         return () => {
-            window.removeEventListener('scroll', changeColor);
+            window.removeEventListener('scroll', handleScroll);
         };
     }, []);
 
@@ -29,21 +35,21 @@ const NavBar = () => {
         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
         }
-        }
+    };
 
     return (
-        <nav className={`z-10 w-full h-[123px] lg:h-[88px] flex flex-col justify-center items-center px-[10px] sm:px-[20px] lg:px-0 fixed transition-colors duration-300 ease-in-out ${color}`}>
+        <nav className={`z-10 w-full ${scrolling ? 'h-[64px] lg:h-[88px]' : 'h-[123px] lg:h-[88px]'} flex flex-col justify-center items-center px-[10px] sm:px-[20px] lg:px-0 fixed transition-all md:transition-colors  duration-300 ease-in-out ${color}`}>
             <div className='w-full flex flex-col justify-center items-center gap-[12px]'>
-                {/*Multilingual-sm-Screen*/}
-                <div className='w-full flex flex-col justify-center items-end lg:hidden'>
+                {/* Multilingual-sm-Screen */}
+                <div className={`w-full flex flex-col justify-center items-end lg:hidden transition-opacity duration-300 ${hideMultilingual ? 'hidden' : 'lg:flex'}`}>
                     <div className='flex flex-row gap-2'>
                         <Link className='text-[15px] underline' href={'/en/'}>English</Link>
                         <Link className='text-[15px] underline' href={'/ar/'}>العربية</Link>
                     </div>
                 </div>
                 <div className="w-full flex flex-row justify-between lg:justify-center items-center lg:gap-[216px] xl:gap-[598px]">
-                    {/*Logo*/}
-                    <div className="w-[114px] h-[50px] lg:w-[147px] lg:h-[64px] hidden sm:flex relative">
+                    {/* Logo */}
+                    <div className={`${scrolling ? 'w-[114px] h-[40px] lg:w-[120px] lg:h-[50px]' : 'w-[114px] h-[50px] lg:w-[147px] lg:h-[64px]'} transition-all duration-300 hidden sm:flex relative`}>
                         <Image
                             src={"/assets/images/budggy-primary-logo.png"}
                             layout="fill"
@@ -61,32 +67,32 @@ const NavBar = () => {
                             alt='budggy-logo'
                         />
                     </div>
-                    {/*Details*/}
+                    {/* Details */}
                     <div className="flex flex-row gap-[24px]">
-                        {/*Socials*/}
+                        {/* Socials */}
                         <div className="hidden sm:flex flex-row justify-center items-center gap-[6px]">
                             <div>
                                 <h2 className="text-black font-semibold text-[14px]">Follow us on</h2>
                             </div>
                             <div className="flex flex-row gap-[16px] justify-center items-center">
                                 <a href="https://www.linkedin.com/company/budggy/" className='text-[#050505] hover:text-[#F79400] transition-colors duration-300'>
-                                    <FaLinkedin size={21}/>
+                                    <FaLinkedin size={21} />
                                 </a>
                                 <a href="https://twitter.com/BudggyHQ" className='text-[#050505] hover:text-[#F79400] transition-colors duration-300'>
-                                    <FaXTwitter size={21}/>
+                                    <FaXTwitter size={21} />
                                 </a>
                                 <a href="https://www.instagram.com/budggyhq/" className='text-[#050505] hover:text-[#F79400] transition-colors duration-300'>
-                                    <FaInstagram size={23}/>
+                                    <FaInstagram size={23} />
                                 </a>
                             </div>
                         </div>
-                        {/*Button*/}
+                        {/* Button */}
                         <div>
-                            <button className="bg-[#F79400] hover:bg-[#be8632] transition-colors duration-300 ease-in-out w-[142px] h-[51px] rounded-xl text-white text-[16px] font-medium" onClick={() => handleClickScroll('contact')}>
+                            <button className="bg-[#F79400] hover:bg-[#be8632] transition-colors duration-300 ease-in-out w-[114px] h-[31px] md:w-[142px] md:h-[51px] rounded-xl text-white text-[16px] font-medium" onClick={() => handleClickScroll('contact')}>
                                 Get in Touch
                             </button>
                         </div>
-                        {/*Multilingual-lg-Screen*/}
+                        {/* Multilingual-lg-Screen */}
                         <div className='hidden lg:flex'>
                             <DropDown />
                         </div>
